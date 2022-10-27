e68524a6ec67238b76d6925fa4807e0b0ab9a895
tadathy <115755786+tadathy@users.noreply.github.com>
2022-10-27 09:15:00 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,38 @@ self.addEventListener('push', function (event) {
 });
 
 self.addEventListener('notificationclick', function (event) {
-  event.notification.close();
-  if (clients.openWindow && event.notification.data.url) {
-    event.waitUntil(clients.openWindow(event.notification.data.url));
-  }
+//  event.notification.close();
+//  if (clients.openWindow && event.notification.data.url) {
+//    event.waitUntil(clients.openWindow(event.notification.data.url));
+//  }
+event.waitUntil(clients.matchAll({
+    type: "window",
+    includeUncontrolled: true
+}).then(function (clientList) {
+    if (event.notification.data.url) {
+        let client = null;
+
+        for (let i = 0; i < clientList.length; i++) {
+            let item = clientList[i];
+
+            if (item.url) {
+                client = item;
+                break;
+            }
+        }
+
+        if (client && 'navigate' in client) {
+            client.focus();
+            event.notification.close();
+            return client.navigate(event.notification.data.url);
+        }
+        else {
+            event.notification.close();
+            // if client doesn't have navigate function, try to open a new browser window
+            return clients.openWindow(event.notification.data.url);
+        }
+    }
+}));
 });
 
 self.addEventListener('install', (event) => {
